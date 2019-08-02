db77e42790f38dc39cf56c9170a626184fee2da6
ludgeromascarenhas <ludgero.mascarenhas@ntconsult.com.br>
2019-08-01 22:32:47 -0300

    inital commit and Kingfisher pod for download images.

---
+++ WoopEventsApp/WoopEventsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WoopEventsApp
+//
+//  Created by Ludgero Gil Mascarenhas on 01/08/19.
+//  Copyright © 2019 Ludgero Gil Mascarenhas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WoopEventsApp/WoopEventsAppTests/WoopEventsAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  WoopEventsAppTests.swift
+//  WoopEventsAppTests
+//
+//  Created by Ludgero Gil Mascarenhas on 01/08/19.
+//  Copyright © 2019 Ludgero Gil Mascarenhas. All rights reserved.
+//
+
+import XCTest
+@testable import WoopEventsApp
+
+class WoopEventsAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
